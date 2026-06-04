199a56e438002cf89dbf927b508c1b702bb136dd
Dima Kuzmenko <kuzmenkodmitry@gmail.com>
2026-06-05 01:35:50 +0300

    Homepage: hero badge (blinking square), underline service tabs, glass cards with corner Expand

--- components/blocks/what-we-do/what-we-do-item.php
+++ components/blocks/what-we-do/what-we-do-item.php
@@ -10,6 +10,13 @@ $modal_title = $item['modal_title'];
     <?php if($image): ?>
         <?php lazy_attachment($image, 'full'); ?>
     <?php endif; ?>
+
+    <?php if ($modal_title && $service_index !== null): ?>
+        <button class="what-we-do-item__btn js-modal-open" data-modal="what-we-do" data-services-source="what-we-do-json" data-service-index="<?php echo $service_index; ?>" type="button" aria-label="Expand">
+            <?php echo inline_svg('icons/expand.svg'); ?>
+        </button>
+    <?php endif; ?>
+
     <div class="what-we-do-item__info">
         <div>
             <h3><?php echo $title; ?></h3>
@@ -18,14 +25,5 @@ $modal_title = $item['modal_title'];
                 <div class="what-we-do-item__desc"><?php echo $description; ?></div>
             <?php endif; ?>
         </div>
-
-        <?php if ($modal_title && $service_index !== null): ?>
-            <button class="what-we-do-item__btn js-modal-open" data-modal="what-we-do" data-services-source="what-we-do-json" data-service-index="<?php echo $service_index; ?>" type="button">
-                <span class="what-we-do-item__btn-icon">
-                    <?php echo inline_svg('icons/expand.svg'); ?>
-                </span>
-                <span class="what-we-do-item__btn-title">Expand</span>
-            </button>
-        <?php endif; ?>
     </div>
 </div>
