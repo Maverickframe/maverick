dcb02a70d613c8b870b78db9d65b74a3e343229c
Maverickframe <kuzmenkodmitry@gmail.com>
2026-07-09 15:24:43 +0300

    production-process → CSS scroll-snap (.mfs-snap mobile-only; desktop grid, ведро A)

--- components/blocks/production-process/production-process.php
+++ components/blocks/production-process/production-process.php
@@ -12,32 +12,32 @@
     <div class="container">
         <?php if(have_rows('items')): ?>
             <div class="production-process__items">
-                <div class="js-production-process-slider splide" role="group" aria-label="<?php the_field('title'); ?> Slider">
-                    <div class="splide__track">
-                        <ul class="splide__list">
-                            <?php
-                                while( have_rows('items')) : the_row();
-                                    $title = get_sub_field('title');
-                                    $description = get_sub_field('description');
-                                    $image_id = get_sub_field('image');
-                            ?>
-                                <li class="js-reveal splide__slide">
-                                    <div class="production-process-item">
-                                        <div>
-                                            <p class="production-process-item__title"><?php echo $title; ?></p>
-                                            <div class="production-process-item__desc"><?php echo $description; ?></div>
-                                        </div>
-
-                                        <div class="production-process-item__img">
-                                            <?php lazy_attachment($image_id, 'large'); ?>
-                                        </div>
+                <div class="mfs-snap" role="group" aria-label="<?php the_field('title'); ?> Slider">
+                    <ul class="mfs-snap__track">
+                        <?php
+                            while( have_rows('items')) : the_row();
+                                $title = get_sub_field('title');
+                                $description = get_sub_field('description');
+                                $image_id = get_sub_field('image');
+                        ?>
+                            <li class="js-reveal mfs-snap__item">
+                                <div class="production-process-item">
+                                    <div>
+                                        <p class="production-process-item__title"><?php echo $title; ?></p>
+                                        <div class="production-process-item__desc"><?php echo $description; ?></div>
                                     </div>
-                                </li>
-                            <?php
-                                endwhile; 
-                            ?>
-                        </ul>
-                    </div>
+
+                                    <div class="production-process-item__img">
+                                        <?php lazy_attachment($image_id, 'large'); ?>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php
+                            endwhile; 
+                        ?>
+                    </ul>
+
+                    <div class="mfs-snap__dots"></div>
                 </div>
             </div>
         <?php
